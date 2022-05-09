33e71ec95e9524b7eda398b575d59d13df34d17c
Axel Maza <axel.maza07@gmail.com>
2022-05-09 22:58:53 +0200

    First creation of network : to debug.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from torchvision import datasets, transforms
 import matplotlib.pyplot as plt
 from SEGDataset import SEGDataset
 import json
+from NeuralNetwork import UNet
 
 def show_image_masks(img, masks):
     fig, axs = plt.subplots(2)
@@ -14,19 +15,25 @@ def show_image_masks(img, masks):
 def train_test_split(prop_of_train=0.9):
     with open('data/kavsir_bboxes.json', 'r') as json_file:
         json_dict = json.load(json_file)
-    print(json_dict)
     name_list = list(json_dict.keys())
     number_of_train=int(prop_of_train*len(name_list))
     train_names, test_names = torch.utils.data.random_split(name_list,[number_of_train, len(name_list)-number_of_train])
     return train_names, test_names
 
 def main():
+    # Showing images
     train_names, test_names = train_test_split(0.8)
     train_data= SEGDataset('data',train_names, transforms.ToTensor())
     n_data=0
     img, masks=train_data.__getitem__(n_data)
-    show_image_masks(img, masks)
+    # show_image_masks(img, masks)
 
+    #Creating a Network
+    x = torch.randn(size=(1, 1, 512, 512), dtype=torch.float32)
+    print(x.shape)
+    print(img.shape)
+    Unet_network=UNet(in_channels=1)
+    print(Unet_network.forward(x).shape)
 
 if __name__ == '__main__':
     main()
