8f60627121110b227f00bc5e76de7b6b6f261219
Axel Maza <axel.maza07@gmail.com>
2022-05-09 12:42:38 +0200

    Add Dataset class that load the data set to be ready to use it.

---
+++ SEGDataset.py
@@ -0,0 +1,79 @@
+import os
+import numpy as np
+import torch
+from PIL import Image
+
+# Found there : #https://pytorch.org/tutorials/intermediate/torchvision_tutorial.html
+class SEGDataset(torch.utils.data.Dataset):
+    def __init__(self, root, transforms=None):
+        self.root = root
+        self.transforms = transforms
+        # load all image files, sorting them to
+        # ensure that they are aligned
+        self.imgs = list(sorted(os.listdir(os.path.join(root, "images"))))
+        self.masks = list(sorted(os.listdir(os.path.join(root, "masks"))))
+
+    def __getitem__(self, idx):
+        # load images and masks
+        img_path = os.path.join(self.root, "images", self.imgs[idx])
+        mask_path = os.path.join(self.root, "masks", self.masks[idx])
+        img = Image.open(img_path).convert("RGB")
+        # note that we haven't converted the mask to RGB,
+        # because each color corresponds to a different instance
+        # with 0 being background
+        mask = Image.open(mask_path)
+        # convert the PIL Image into a numpy array
+        mask = np.array(mask)
+        mask = np.array(mask<50,dtype=int) #Make the white part = 1 and the black part = 0
+
+        #Resize :
+        img = np.resize(img,(500,500))
+        mask = np.resize(mask, (500, 500))
+        # instances are encoded as different colors
+        obj_ids = np.unique(mask)
+
+        # first id is the background, so remove it
+        obj_ids = obj_ids[1:]
+
+        # split the color-encoded mask into a set
+        # of binary masks
+        masks = mask == obj_ids[:, None, None]
+        # get bounding box coordinates for each mask
+        num_objs = len(obj_ids)
+
+        boxes = []
+        for i in range(num_objs):
+            pos = np.where(masks[i])
+            xmin = np.min(pos[1])
+            xmax = np.max(pos[1])
+            ymin = np.min(pos[0])
+            ymax = np.max(pos[0])
+            boxes.append([xmin, ymin, xmax, ymax])
+
+        # convert everything into a torch.Tensor
+        boxes = torch.as_tensor(boxes, dtype=torch.float32)
+        # there is only one class
+        labels = torch.ones((num_objs,), dtype=torch.int64)
+        masks = torch.as_tensor(masks, dtype=torch.uint8)
+
+        image_id = torch.tensor([idx])
+        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+        # suppose all instances are not crowd
+        iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
+
+        target = {}
+        target["boxes"] = boxes
+        target["labels"] = labels
+        target["masks"] = masks
+        target["image_id"] = image_id
+        target["area"] = area
+        target["iscrowd"] = iscrowd
+
+        if self.transforms is not None:
+            # img, target = self.transforms(img, target)
+            img= self.transforms(img)
+        return img, target
+
+    def __len__(self):
+        return len(self.imgs)
+
--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 #!/usr/bin/env python3
+import torch
+import numpy as np
+from torch.utils.data import Dataset
+from torchvision import datasets, transforms
+import matplotlib.pyplot as plt
+import json
+from SEGDataset import SEGDataset
+
+
+def get_transform():
+    transform = transforms.Compose([transforms.ToTensor(),
+                                    transforms.Resize(255),
+                                    transforms.CenterCrop(224)])
+    return transform
 
 def main():
-    print('Hello world!')
+
+    data= SEGDataset('data',transforms.ToTensor())
+    # print(data.__len__())
+    print(data.__getitem__(2))
 
 if __name__ == '__main__':
     main()
+
+
+
+
