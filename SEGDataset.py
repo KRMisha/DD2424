bf481187fb87c7e1572c0b60fe8576c9983e47d4
Axel Maza <axel.maza07@gmail.com>
2022-05-09 16:44:44 +0200

    Splitting train / test data before loading

--- SEGDataset.py
+++ SEGDataset.py
@@ -2,25 +2,23 @@ import os
 import numpy as np
 import torch
 from PIL import Image
-import json
+
 
 # Found there : #https://pytorch.org/tutorials/intermediate/torchvision_tutorial.html
 
 #Constants
-img_size=500
+img_size=512
 
 
 class SEGDataset(torch.utils.data.Dataset):
-    def __init__(self, root, transforms=None):
+    def __init__(self, root, name_list, transforms=None):
         self.root = root
         self.transforms = transforms
         # load all image files, sorting them to
         # ensure that they are aligned
-        with open('data/kavsir_bboxes.json', 'r') as json_file:
-            json_dict = json.load(json_file)
 
-        self.imgs=list(map(lambda name : name+'.jpg', list(json_dict.keys())))
-        self.masks = list(map(lambda name: name + '.jpg', list(json_dict.keys())))
+        self.imgs=list(map(lambda name : name+'.jpg', name_list))
+        self.masks = list(map(lambda name: name + '.jpg', name_list))
 
 
 
@@ -48,38 +46,38 @@ class SEGDataset(torch.utils.data.Dataset):
         # of binary masks
         masks = mask == obj_ids[:, None, None]
         # get bounding box coordinates for each mask
-        num_objs = len(obj_ids)
-        boxes = []
-        for i in range(num_objs):
-            pos = np.where(masks[i])
-            xmin = np.min(pos[1])
-            xmax = np.max(pos[1])
-            ymin = np.min(pos[0])
-            ymax = np.max(pos[0])
-            boxes.append([xmin, ymin, xmax, ymax])
+        # num_objs = len(obj_ids)
+        # boxes = []
+        # for i in range(num_objs):
+        #     pos = np.where(masks[i])
+        #     xmin = np.min(pos[1])
+        #     xmax = np.max(pos[1])
+        #     ymin = np.min(pos[0])
+        #     ymax = np.max(pos[0])
+        #     boxes.append([xmin, ymin, xmax, ymax])
         # convert everything into a torch.Tensor
-        boxes = torch.as_tensor(boxes, dtype=torch.float32)
+        # boxes = torch.as_tensor(boxes, dtype=torch.float32)
         # there is only one class
-        labels = torch.ones((num_objs,), dtype=torch.int64)
+        # labels = torch.ones((num_objs,), dtype=torch.int64)
         masks = torch.as_tensor(masks, dtype=torch.uint8)
 
-        image_id = torch.tensor([idx])
-        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+        # image_id = torch.tensor([idx])
+        # area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
         # suppose all instances are not crowd
-        iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
+        # iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
 
-        target = {}
-        target["boxes"] = boxes
-        target["labels"] = labels
-        target["masks"] = masks
-        target["image_id"] = image_id
-        target["area"] = area
-        target["iscrowd"] = iscrowd
+        # target = {}
+        # target["boxes"] = boxes
+        # target["labels"] = labels
+        # target["masks"] = masks
+        # target["image_id"] = image_id
+        # target["area"] = area
+        # target["iscrowd"] = iscrowd
 
         if self.transforms is not None:
             # img, target = self.transforms(img, target)
             img= self.transforms(img)
-        return img, target
+        return img, masks
 
     def __len__(self):
         return len(self.imgs)
--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 #!/usr/bin/env python3
 import torch
-import numpy as np
 from torchvision import datasets, transforms
 import matplotlib.pyplot as plt
 from SEGDataset import SEGDataset
+import json
 
-def show_image_masks(img, target):
+def show_image_masks(img, masks):
     fig, axs = plt.subplots(2)
     axs[0].imshow(img.permute(1, 2, 0))
-    axs[1].imshow(((-1 * target['masks']) + 1) * 255)
+    axs[1].imshow(((-1 * masks) + 1) * 255)
     plt.show()
 
+def train_test_split(prop_of_train=0.9):
+    with open('data/kavsir_bboxes.json', 'r') as json_file:
+        json_dict = json.load(json_file)
+    print(json_dict)
+    name_list = list(json_dict.keys())
+    number_of_train=int(prop_of_train*len(name_list))
+    train_names, test_names = torch.utils.data.random_split(name_list,[number_of_train, len(name_list)-number_of_train])
+    return train_names, test_names
+
 def main():
-    data= SEGDataset('data',transforms.ToTensor())
+    train_names, test_names = train_test_split(0.8)
+    train_data= SEGDataset('data',train_names, transforms.ToTensor())
     n_data=0
-    img, target=data.__getitem__(n_data)
-    show_image_masks(img, target)
+    img, masks=train_data.__getitem__(n_data)
+    show_image_masks(img, masks)
 
 
 if __name__ == '__main__':
