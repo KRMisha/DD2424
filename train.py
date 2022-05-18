5329a3c5fed533b9ae888d70f3b060e1452a99b4
Misha Krieger-Raynauld <mkriegerraynauld@gmail.com>
2022-05-18 15:47:57 +0200

    Fix whitespace issues in train.py

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import torch
 import config
 
+
 def train(dataloader, model, loss_fn, optimizer):
     model.train()
 
@@ -23,6 +24,7 @@ def train(dataloader, model, loss_fn, optimizer):
     # Return average loss
     return total_loss / len(dataloader)
 
+
 def valid(dataloader, model, loss_fn):
     model.eval()
 
@@ -34,6 +36,6 @@ def valid(dataloader, model, loss_fn):
             X, y = X.to(config.DEVICE), y.to(config.DEVICE)
             pred = model(X)
             total_loss += loss_fn(pred, y).item()
-    
+
     # Return average loss
     return total_loss / len(dataloader)
