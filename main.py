82d09cc56e08f8f86285246fe364817c65abd419
Misha Krieger-Raynauld <mkriegerraynauld@gmail.com>
2022-05-21 01:16:35 +0200

    Add printing of configuration information on startup

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ import functions # TODO: Reconsider
 from model import UNet
 from test import test
 from train import train, valid
-from diceloss import DiceLoss
 
 
 def main():
@@ -19,16 +18,21 @@ def main():
     config.OUTPUT_DIRECTORY.mkdir(exist_ok=True)
     config.PREDICTED_IMAGES_DIRECTORY.mkdir(exist_ok=True)
 
+    # Print configuration information
     print(f'Using {config.DEVICE} device')
-
-    # Load training dataset and split it into train and validation sets
-    dataset = KvasirSegDataset(root=config.DATASET_DIRECTORY, train=True, transform=functions.transforms)
-    train_dataset, valid_dataset = random_split(dataset, [config.TRAIN_DATASET_SIZE, config.VALID_DATASET_SIZE])
     print(
+        f'Dataset sizes: '
         f'Training: {config.TRAIN_DATASET_SIZE} samples | '
         f'Validation: {config.VALID_DATASET_SIZE} samples | '
         f'Test: {config.TEST_DATASET_SIZE} samples'
     )
+    print(f'Hyperparameters: Learning rate: {config.LEARNING_RATE} | Batch size: {config.BATCH_SIZE}')
+    print(f'Model parameters: First layer channels: {config.ENCODER_CHANNELS[1]} | Layers: {len(config.ENCODER_CHANNELS) - 1}')
+    print(f'Input image dimensions: {config.INPUT_IMAGE_DIMENSIONS}')
+
+    # Load training dataset and split it into train and validation sets
+    dataset = KvasirSegDataset(root=config.DATASET_DIRECTORY, train=True, transform=functions.transforms)
+    train_dataset, valid_dataset = random_split(dataset, [config.TRAIN_DATASET_SIZE, config.VALID_DATASET_SIZE])
 
     # Create data loaders
     train_dataloader = DataLoader(
