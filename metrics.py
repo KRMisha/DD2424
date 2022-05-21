be8cff42ca088370b3ac3140a32f286aa0943028
Misha Krieger-Raynauld <mkriegerraynauld@gmail.com>
2022-05-22 00:57:50 +0200

    Add newline between free functions

--- metrics.py
+++ metrics.py
@@ -5,6 +5,7 @@ def dice_coefficient(input, target):
     intersection = torch.sum(input * target)
     return 2 * intersection / (torch.sum(input) + torch.sum(target))
 
+
 def iou(input, target):
     intersection = torch.sum(input * target)
     union = torch.sum(input) + torch.sum(target) - intersection
