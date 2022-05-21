0358e0123ac2037925d3a5c89a29ce664f5ab246
Misha Krieger-Raynauld <mkriegerraynauld@gmail.com>
2022-05-21 22:54:53 +0200

    Add shear transformation to data augmentations

--- transforms.py
+++ transforms.py
@@ -9,5 +9,6 @@ augmentations = T.Compose([
     T.RandomVerticalFlip(p=0.5),
     T.RandomHorizontalFlip(p=0.5),
     T.RandomApply([T.RandomAffine(degrees=(0, 45), translate=(0, 0.2), scale=(0.8, 1))], p=0.3),
+    T.RandomApply([T.RandomAffine(degrees=0, shear=(0, 30))], p=0.7),
     T.RandomApply([T.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5)], p=0.5),
 ])
