05d9031cdc0051f95fdee32c105b7c09ab24a03e
Misha Krieger-Raynauld <mkriegerraynauld@gmail.com>
2022-05-21 21:43:21 +0200

    Apply different transformations to each sample in batch

--- train.py
+++ train.py
@@ -11,11 +11,12 @@ def train(dataloader, model, loss_fn, optimizer, transform=None):
         X, y = X.to(config.DEVICE), y.to(config.DEVICE)
 
         # Apply data transformations using GPU
-        if transform is not None:
-            rng_state = torch.get_rng_state()
-            X = transform(X)
-            torch.set_rng_state(rng_state)
-            y = transform(y)
+        for i in range(len(X)):
+            if transform is not None:
+                rng_state = torch.get_rng_state()
+                X[i] = transform(X[i])
+                torch.set_rng_state(rng_state)
+                y[i] = transform(y[i])
 
         # Compute prediction error
         pred = model(X)
